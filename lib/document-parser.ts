66713ee2153b7d1dfd14749260abf0ece09cb538
Jai0401 <21cs3025@rgipt.ac.in>
2025-03-09 00:59:36 +0530

    Add PDF parsing functionality and initialize worker in document parser

--- lib/document-parser.ts
+++ lib/document-parser.ts
@@ -38,6 +38,7 @@ async function parseText(buffer: ArrayBuffer): Promise<string> {
 
 async function parseImage(file: File): Promise<string> {
   const worker = await createWorker()
+  await worker.load()
   await worker.loadLanguage("eng")
   await worker.initialize("eng")
 
